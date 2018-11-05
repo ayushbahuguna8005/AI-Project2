38bb458a463985c5461a51fca2aad410f0a40b50
Ayush Bahuguna <ayushbahuguna8005@gmail.com>
2018-11-04 23:43:09 -0500

    second commit

--- DecisionTreeImpl.py
+++ DecisionTreeImpl.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn import preprocessing
-from sklearn.naive_bayes import ComplementNB
 from sklearn.metrics import accuracy_score
+from sklearn import tree
 
 df = pd.read_csv('DataSet-Release 1/ds1/ds1Info.csv')
 df_alpha_numeric = pd.read_csv('DataSet-Release 1/ds1/ds1Train.csv')
@@ -19,20 +16,23 @@ df_greek_valid = pd.read_csv('DataSet-Release 1/ds2/ds2Val.csv')
 train_X = df_alpha_numeric.ix[:, :1024]
 train_Y = df_alpha_numeric.ix[:, 1024]
 
-#trainXScaled = preprocessing.scale(train_X)
-#trainYScaled = preprocessing.scale(train_Y)
-
-naive_bayes_classifier = BernoulliNB()
-naive_bayes_classifier.fit(train_X, train_Y)
-#classifier.fit(trainXScaled, trainYScaled)
 
 testX = df_alpha_numeric_validation.ix[:, :1024]
 targetTest = df_alpha_numeric_validation.ix[:, 1024]
 
-print('Dataset 1')
-predictedTarget = naive_bayes_classifier.predict(train_X)
+#---------------------------------------------------------------
+# max_depths = np.linspace(1, 32, 32, endpoint=True)
+# min_samples_splits = np.linspace(0.1, 1.0, 10, endpoint=True)
 
-print(accuracy_score(predictedTarget, train_Y))
+clf = tree.DecisionTreeClassifier(criterion="gini")
+# for min_samples_split in min_samples_splits:
+#    dt = tree.DecisionTreeClassifier(min_samples_split=min_samples_split)
+#    dt.fit(train_X, train_Y)
+# clf = tree.DecisionTreeClassifier(max_depth=max_depths)
+clf.fit(train_X, train_Y)
+clfTarget = clf.predict(testX)
+print(accuracy_score(targetTest, clfTarget))
+
 
 train_GreekX = df_greek.ix[:, :1024]
 train_GreekY = df_greek.ix[:, 1024]
@@ -40,7 +40,6 @@ train_GreekY = df_greek.ix[:, 1024]
 testGreekX = df_greek_valid.ix[:, :1024]
 testGreekTarget = df_greek_valid.ix[:, 1024]
 
-naive_bayes_classifier.fit(train_GreekX, train_GreekY)
-predictGreek = naive_bayes_classifier.predict(testGreekX)
-print('Dataset 2')
-print(accuracy_score(testGreekTarget, predictGreek))
+clf.fit(train_GreekX, train_GreekY)
+clfTarget = clf.predict(testGreekX)
+print(accuracy_score(testGreekTarget, clfTarget))
